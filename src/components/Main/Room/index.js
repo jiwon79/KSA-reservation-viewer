01bb2e723b1fd940b6f10e1674aaafefa7cee627
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 06:06:36 +0900

    feat: Add time in main page

--- src/components/Main/Room/index.js
+++ src/components/Main/Room/index.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-array-constructor */
 import React from 'react'
 import './style.css'
 
 export default function Room({room, name, number}) {
   console.log(name, number);
+  const timeList = Array('1', '2', '3', '4', '점심', '5', '6', '7', '8', '9', '저녁', '10', '11', '12', '13');
   const list = name.map((name, index) => {
-    if (name === '-') return <div className="box empty"></div>
-    return <div className="box fill">{name}<br/>{number[index]}</div>
+    if (name === '-') return <div className="box empty"><div className="time">{timeList[index]}</div></div>
+    return (
+      <div className="box fill">
+        <div className="time">{timeList[index]}</div>
+        {name}<br/>{number[index]}
+      </div>
+    )
   });
 
   return (
