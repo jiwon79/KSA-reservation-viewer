8d5b8a21f3ecd6a09dc4572c672ebf6208261c90
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 04:22:01 +0900

    feat: Add Room component styles

--- src/components/Main/Room/index.js
+++ src/components/Main/Room/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './style.css'
 
-export default function Room({name, number}) {
+export default function Room({room, name, number}) {
   console.log(name, number);
   return (
-    <div>
-      Room Data
+    <div className="room">
+      <div className="room__header">
+        <span>Room {room}</span>
+        <div className="colorBox1"></div>
+        <span>예약되지 않음</span>
+        <div className="colorBox2"></div>
+        <span>예약됨</span>
+      </div>
+      <div>
+        {number}
+      </div>
     </div>
   )
 }
