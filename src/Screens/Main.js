f4902aaa6852a7c2cd4270d422b43b951f0a5c8b
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 03:00:56 +0900

    feat: Add Header in Main page

--- src/Screens/Main.js
+++ src/Screens/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
+import Header from '../components/Main/Header/'
+
 export default function Main() {
   return (
     <div>
-      mainsdaasdfdfs
-      한긇ㄴ한글
+      <Header />
     </div>
   )
 }
