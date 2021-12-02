67c0e40cd0c726eb795e0090003ec72e62373f99
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 05:08:47 +0900

    feat: Add roomList grid style

--- src/components/Main/Room/index.js
+++ src/components/Main/Room/index.js
@@ -3,6 +3,11 @@ import './style.css'
 
 export default function Room({room, name, number}) {
   console.log(name, number);
+  const list = name.map((name, index) => {
+    if (name === '-') return <div className="box empty"></div>
+    return <div className="box fill">{name}<br/>{number[index]}</div>
+  });
+
   return (
     <div className="room">
       <div className="room__header">
@@ -12,8 +17,8 @@ export default function Room({room, name, number}) {
         <div className="colorBox2"></div>
         <span>예약됨</span>
       </div>
-      <div>
-        {number}
+      <div className="room__list">
+        {list}
       </div>
     </div>
   )
