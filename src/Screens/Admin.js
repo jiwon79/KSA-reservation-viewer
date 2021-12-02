bc41f33b3fc5bdcf3dfb652eed3579d371e3ef13
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 05:27:11 +0900

    feat: Add ul in admin page

--- src/Screens/Admin.js
+++ src/Screens/Admin.js
@@ -7,8 +7,10 @@ export default function Admin() {
   return (
     <div>
       <Header/>
-      <Link to="/log">로그</Link>
-      <Link to="/teacher">선생님 에약 페이지</Link>
+      <ul>
+        <li><Link to="/teacher">선생님 에약 페이지</Link></li>
+        <li><Link to="/log">로그</Link></li>
+      </ul>
     </div>
   )
 }
