3300bc7b2ca75c0e216edb0b43b2d9eb1dea07db
jiwon79 <19-079@ksa.hs.kr>
2021-12-03 04:22:12 +0900

    feat: Add RoomList component styels

--- src/components/Main/RoomList/index.js
+++ src/components/Main/RoomList/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Room from '../Room';
 import { Reservation } from '../../../common/api';
+import "./style.css"
 
 export default function RoomList() {
   const [reserveData, setReserveData] = useState('');
@@ -13,18 +14,18 @@ export default function RoomList() {
   }, [])
 
   return (
-    <div>
+    <div className="roomList">
       {
         reserveData === '' 
         ? 
         '로딩중' 
         : 
         <>
-          <Room name={reserveData.name.a} number={reserveData.number.a}/>
-          <Room name={reserveData.name.b} number={reserveData.number.b}/>
-          <Room name={reserveData.name.c} number={reserveData.number.c}/>
-          <Room name={reserveData.name.d} number={reserveData.number.d}/>
-          <Room name={reserveData.name.e} number={reserveData.number.e}/>
+          <Room room={'A'} name={reserveData.name.a} number={reserveData.number.a}/>
+          <Room room={'B'} name={reserveData.name.b} number={reserveData.number.b}/>
+          <Room room={'C'} name={reserveData.name.c} number={reserveData.number.c}/>
+          <Room room={'D'} name={reserveData.name.d} number={reserveData.number.d}/>
+          <Room room={'E'} name={reserveData.name.e} number={reserveData.number.e}/>
         </>
       }
     </div>
